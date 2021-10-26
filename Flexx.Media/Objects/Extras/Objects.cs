4908a98535f349528118d7cca15efe46f7714328
Drew Chase <dcmanproductions@gmail.com>
2021-10-26 01:52:32 -0400

    Added Unwatched and UpNext properties

--- Flexx.Media/Objects/Extras/Objects.cs
+++ Flexx.Media/Objects/Extras/Objects.cs
@@ -11,17 +11,17 @@ namespace Flexx.Media.Objects.Extras
 {
     public class MovieObject
     {
-        public string ID { get; private set; }
-        public string Title { get; private set; }
-        public string Plot { get; private set; }
-        public string MPAA { get; private set; }
-        public double Rating { get; private set; }
-        public bool Downloaded { get; private set; }
-        public ushort Year { get; private set; }
-        public string Poster { get; private set; }
-        public string Cover { get; private set; }
-        public bool Watched { get; private set; }
-        public ushort WatchedDuration { get; private set; }
+        public string ID { get; }
+        public string Title { get; }
+        public string Plot { get; }
+        public string MPAA { get; }
+        public double Rating { get; }
+        public bool Downloaded { get; }
+        public ushort Year { get; }
+        public string Poster { get; }
+        public string Cover { get; }
+        public bool Watched { get; }
+        public ushort WatchedDuration { get; }
 
         public MovieObject(MovieModel movie, User user)
         {
@@ -44,7 +44,8 @@ namespace Flexx.Media.Objects.Extras
             ID = result["id"].ToString();
             Title = result["title"].ToString();
             Plot = result["overview"].ToString();
-            Year = (ushort)DateTime.Parse(result["release_date"].ToString()).Year;
+            if (DateTime.TryParse(result["release_date"].ToString(), out DateTime date))
+                Year = (ushort)date.Year;
             Poster = $"https://image.tmdb.org/t/p/original{result["poster_path"]}";
             Cover = $"https://image.tmdb.org/t/p/original{result["backdrop_path"]}";
             Rating = double.Parse(result["vote_average"].ToString());
@@ -67,68 +68,28 @@ namespace Flexx.Media.Objects.Extras
                 }
             }
         }
+    }
 
-        public class SeriesObject
+    public class SeriesObject
+    {
+        public string ID { get; }
+        public string Title { get; }
+        public string Plot { get; }
+        public DateTime ReleaseDate { get; }
+        public bool Watched { get; }
+        public bool Added { get; }
+
+        public object UpNext { get; }
+
+        public SeriesObject(TVModel show, User user)
         {
-            public string ID { get; private set; }
-            public string Title { get; private set; }
-            public string Plot { get; private set; }
-            public DateTime ReleaseDate { get; private set; }
-            public bool Watched { get; private set; }
-            public bool Added { get; private set; }
-
-            public SeriesObject(TVModel show, User user)
+            ID = show.TMDB;
+            Title = show.Title;
+            Plot = show.Plot;
+            ReleaseDate = show.StartDate;
+            Watched = !show.Seasons.Where(s =>
             {
-                ID = show.TMDB;
-                Title = show.Title;
-                Plot = show.Plot;
-                ReleaseDate = show.StartDate;
-                Watched = !show.Seasons.Where(s =>
-                {
-                    return s.Episodes.Where(e =>
-                    {
-                        if (!user.GetHasWatched(e.MetaDataKey))
-                        {
-                            return true;
-                        }
-
-                        return false;
-                    }).Any();
-                }).Any();
-                Added = true;
-            }
-
-            public SeriesObject(string json)
-            {
-                JObject result = (JObject)JsonConvert.DeserializeObject(json);
-                ID = result["id"].ToString();
-                Title = result["name"].ToString();
-                Plot = result["overview"].ToString();
-                ReleaseDate = DateTime.Parse(result["first_air_date"].ToString());
-                Watched = false;
-                Added = false;
-            }
-        }
-
-        public class SeasonObject
-        {
-            public string Name { get; private set; }
-            public string Plot { get; private set; }
-            public DateTime ReleaseDate { get; private set; }
-            public int Season { get; private set; }
-            public int Episodes { get; private set; }
-            public string Show { get; private set; }
-            public bool Watched { get; private set; }
-
-            public SeasonObject(SeasonModel season, User user)
-            {
-                Name = season.Title;
-                Plot = season.Plot;
-                Season = season.Season_Number;
-                Episodes = season.Episodes.Count;
-                ReleaseDate = season.StartDate;
-                Show = season.Series.Title;
-                Watched = !season.Episodes.Where(e =>
+                return s.Episodes.Where(e =>
                 {
                     if (!user.GetHasWatched(e.MetaDataKey))
                     {
@@ -137,65 +98,147 @@ namespace Flexx.Media.Objects.Extras
 
                     return false;
                 }).Any();
-            }
-
-            public SeasonObject(string json)
+            }).Any();
+            Added = true;
+            SeasonModel[] UnwatchedSeasons = show.Seasons.Where(s => !new SeasonObject(s, user).Watched && new SeasonObject(s, user).Season != 0).ToArray();
+            SeasonModel UnwatchedSeason = UnwatchedSeasons.Length == 0 ? show.Seasons.ElementAt(0) : UnwatchedSeasons[0];
+            EpisodeModel[] UnwatchedEpisodes = UnwatchedSeason.Episodes.Where(e => !new EpisodeObject(e, user).Watched && new EpisodeObject(e, user).Downloaded).ToArray();
+            EpisodeModel UnwatchedEpisode = UnwatchedEpisodes.Length == 0 ? UnwatchedSeason.Episodes.ElementAt(0) : UnwatchedEpisodes[0];
+            UpNext = new
             {
-                JObject result = (JObject)JsonConvert.DeserializeObject(json);
-                Name = result["name"].ToString();
-                Plot = result["overview"].ToString();
-                ReleaseDate = DateTime.Parse(result["air_date"].ToString());
-                Watched = false;
-                Episodes = 0;
-            }
+                Season = UnwatchedEpisode.Season.Season_Number,
+                Episode = UnwatchedEpisode.Episode_Number,
+                Name = UnwatchedEpisode.FriendlyName,
+            };
         }
 
-        public class EpisodeObject
+        public SeriesObject(string json)
         {
-            public string ID { get; private set; }
-            public string Title { get; private set; }
-            public string Name { get; private set; }
-            public string Plot { get; private set; }
-            public double Rating { get; private set; }
-            public bool Downloaded { get; private set; }
-            public DateTime ReleaseDate { get; private set; }
-            public string Poster { get; private set; }
-            public int Season { get; private set; }
-            public int Episode { get; private set; }
-            public string Show { get; private set; }
-            public bool Watched { get; private set; }
-            public ushort WatchedDuration { get; private set; }
+            JObject result = (JObject)JsonConvert.DeserializeObject(json);
+            ID = result["id"].ToString();
+            Title = result["name"].ToString();
+            Plot = result["overview"].ToString();
+            if (DateTime.TryParse(result["first_air_date"].ToString(), out DateTime date))
+                ReleaseDate = date;
+            Watched = false;
+            Added = TvLibraryModel.Instance.GetShowByTMDB(ID) != null;
+        }
+    }
 
-            public EpisodeObject(EpisodeModel episode, User user)
-            {
-                ID = episode.Season.Series.TMDB;
-                Title = episode.Title;
-                Name = episode.FriendlyName;
-                Plot = episode.Plot;
-                Rating = episode.Rating;
-                Downloaded = episode.Downloaded;
-                ReleaseDate = episode.ReleaseDate;
-                Poster = episode.PosterImage;
-                Show = episode.Season.Series.Title;
-                Season = episode.Season.Season_Number;
-                Episode = episode.Episode_Number;
-                Watched = user.GetHasWatched(episode.MetaDataKey);
-                WatchedDuration = user.GetWatchedDuration(episode.MetaDataKey);
-            }
+    public class SeasonObject
+    {
+        public string Name { get; }
+        public string Plot { get; }
+        public DateTime ReleaseDate { get; }
+        public int Season { get; }
+        public int Episodes { get; }
+        public string Show { get; }
+        public bool Watched { get; }
+        public object UpNext { get; }
 
-            public EpisodeObject(string json)
+        public SeasonObject(SeasonModel season, User user)
+        {
+            season.Episodes = season.Episodes.OrderBy(e => e.Episode_Number).ToList();
+            Name = season.Title;
+            Plot = season.Plot;
+            Season = season.Season_Number;
+            Episodes = season.Episodes.Count;
+            ReleaseDate = season.StartDate;
+            Show = season.Series.Title;
+            Watched = !season.Episodes.Where(e =>
             {
-                JObject result = (JObject)JsonConvert.DeserializeObject(json);
-                ID = result["id"].ToString();
-                Title = result["name"].ToString();
-                Name = "";
-                Plot = result["overview"].ToString();
-                ReleaseDate = DateTime.Parse(result["air_date"].ToString());
-                Rating = double.Parse(result["vote_average"].ToString());
-                Downloaded = false;
-                Watched = false;
-                WatchedDuration = 0;
-            }
+                if (!user.GetHasWatched(e.MetaDataKey))
+                {
+                    return true;
+                }
+
+                return false;
+            }).Any();
+            EpisodeModel[] UnwatchedEpisodes = season.Episodes.Where(e => !new EpisodeObject(e, user).Watched).ToArray();
+            EpisodeModel UnwatchedEpisode = UnwatchedEpisodes.Length == 0 ? season.Episodes.ElementAt(0) : UnwatchedEpisodes[0];
+            UpNext = new
+            {
+                Season = UnwatchedEpisode.Season.Season_Number,
+                Episode = UnwatchedEpisode.Episode_Number,
+                Name = UnwatchedEpisode.FriendlyName,
+            };
+        }
+
+        public SeasonObject(string json)
+        {
+            JObject result = (JObject)JsonConvert.DeserializeObject(json);
+            Name = result["name"].ToString();
+            Plot = result["overview"].ToString();
+            if (DateTime.TryParse(result["air_date"].ToString(), out DateTime date))
+                ReleaseDate = date;
+            Watched = false;
+            if (result["episode_count"] != null)
+                Episodes = int.TryParse(result["episode_count"].ToString(), out int episode) ? episode : 0;
+            if (result["season_number"] != null)
+                Season = int.TryParse(result["season_number"].ToString(), out int season) ? season : 0;
+        }
+    }
+
+    public class EpisodeObject
+    {
+        public string ID { get; }
+        public string Title { get; }
+        public string Name { get; }
+        public string Plot { get; }
+        public double Rating { get; }
+        public bool Downloaded { get; }
+        public DateTime ReleaseDate { get; }
+        public string Poster { get; }
+        public int Season { get; }
+        public int Episode { get; }
+        public string Show { get; }
+        public bool Watched { get; }
+        public ushort WatchedDuration { get; }
+        public object NextEpisode { get; }
+
+        public EpisodeObject(EpisodeModel episode, User user)
+        {
+            ID = episode.Season.Series.TMDB;
+            Title = episode.Title;
+            Name = episode.FriendlyName;
+            Plot = episode.Plot;
+            Rating = episode.Rating;
+            Downloaded = episode.Downloaded;
+            ReleaseDate = episode.ReleaseDate;
+            Poster = episode.PosterImage;
+            Show = episode.Season.Series.Title;
+            Season = episode.Season.Season_Number;
+            Episode = episode.Episode_Number;
+            Watched = user.GetHasWatched(episode.MetaDataKey);
+            WatchedDuration = user.GetWatchedDuration(episode.MetaDataKey);
+
+            int currentIndex = episode.Season.Episodes.IndexOf(episode);
+            int seasonIndex = episode.Season.Series.Seasons.IndexOf(episode.Season);
+            EpisodeModel next = episode.Season.Episodes.Count <= currentIndex ? episode.Season.Episodes.ElementAt(currentIndex + 1) : episode.Season.Series.Seasons.Count <= seasonIndex ? episode.Season.Series.Seasons.ElementAt(seasonIndex + 1).Episodes.ElementAt(0) : null;
+            if (next != null)
+                NextEpisode = new
+                {
+                    Season = next.Season.Season_Number,
+                    Episode = next.Episode_Number,
+                    Name = next.FriendlyName,
+                };
+        }
+
+        public EpisodeObject(string json)
+        {
+            JObject result = (JObject)JsonConvert.DeserializeObject(json);
+            ID = result["id"].ToString();
+            Title = result["name"].ToString();
+            Season = int.TryParse(result["season_number"].ToString(), out int season) ? season : 0;
+            Episode = int.TryParse(result["episode_number"].ToString(), out int episode) ? episode : 0;
+            Name = $"S{(Season < 10 ? "0" + Season : Season)}E{(Episode < 10 ? "0" + Episode : Episode)}";
+            Plot = result["overview"].ToString();
+            if (DateTime.TryParse(result["air_date"].ToString(), out DateTime date))
+                ReleaseDate = date;
+            Rating = double.Parse(result["vote_average"].ToString());
+            Downloaded = false;
+            Watched = false;
+            WatchedDuration = 0;
         }
     }
 }
