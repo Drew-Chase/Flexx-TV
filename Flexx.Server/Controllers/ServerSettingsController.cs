82d56cff776b8bb7b5cf86dcf6128e04361fef03
Drew Chase <dcmanproductions@gmail.com>
2021-10-27 03:26:14 -0400

    Added Server Settings Controller

---
+++ Flexx.Server/Controllers/ServerSettingsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Flexx.Server.Controllers
+{
+    public class ServerSettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
