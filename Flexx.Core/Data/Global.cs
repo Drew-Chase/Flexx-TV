73af3faa6152e0dc69ddd6b7cec56f0be5a89fd8
Drew Chase <dcmanproductions@gmail.com>
2021-11-24 15:22:32 -0500

    Removed depracated code

--- Flexx.Core/Data/Global.cs
+++ Flexx.Core/Data/Global.cs
@@ -5,138 +5,134 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 
-namespace Flexx.Core.Data
+namespace Flexx.Core.Data;
+public static class Global
 {
-    public static class Global
-    {
-        public static string TMDB_API => "378ae44c6e7f5dde094cd8c8456378e0";
+    public static string TMDB_API => "378ae44c6e7f5dde094cd8c8456378e0";
 #if DEBUG
-        public static ILog log => LogManager.Init().SetLogDirectory(Path.Combine(Paths.Log, "latest.log")).SetDumpMethod(DumpType.NoDump).SetMinimumLogType(Lists.LogTypes.All);
+    public static ILog log => LogManager.Init().SetLogDirectory(Path.Combine(Paths.Log, "latest.log")).SetDumpMethod(DumpType.NoDump).SetMinimumLogType(Lists.LogTypes.All);
 #else
         public static ILog log => LogManager.Init().SetLogDirectory(Path.Combine(Paths.Log, "latest.log")).SetDumpMethod(3000).SetMinimumLogType(Lists.LogTypes.Info);
 #endif
-        public static Configuration config = new();
-        public static string[] Media_Extensions = new string[] { "mpegg", "mpeg", "mp4", "mkv", "m4a", "m4v", "f4v", "f4a", "m4b", "m4r", "f4b", "mov", "3gp", "3gp2", "3g2", "3gpp", "3gpp2", "ogg", "oga", "ogv", "ogx", "wmv", "wma", "flv", "avi" };
+    public static Configuration config = new();
+    public static string[] Media_Extensions = new string[] { "mpegg", "mpeg", "mp4", "mkv", "m4a", "m4v", "f4v", "f4a", "m4b", "m4r", "f4b", "mov", "3gp", "3gp2", "3g2", "3gpp", "3gpp2", "ogg", "oga", "ogv", "ogx", "wmv", "wma", "flv", "avi" };
 
-        public enum DiscoveryCategory
+    public enum DiscoveryCategory
+    {
+        None,
+        Latest,
+        Popular,
+        Top_Rated,
+        Upcoming
+    }
+
+    public static class Paths
+    {
+        public static string Root => Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "LFInteractive", "Flexx")).FullName;
+        public static string Resources => Directory.CreateDirectory(Path.Combine(Root, "Resources")).FullName;
+        public static string Log => Directory.CreateDirectory(Path.Combine(Resources, "Logs")).FullName;
+        public static string Configs => Directory.CreateDirectory(Path.Combine(Resources, "Config")).FullName;
+        public static string UserData => Directory.CreateDirectory(Path.Combine(Resources, "UserData")).FullName;
+        public static string FFMpeg => Directory.CreateDirectory(Path.Combine(Resources, "FFMpeg")).FullName;
+        public static string TempData => Directory.CreateDirectory(Path.Combine(Resources, ".tmp")).FullName;
+
+        public static string TranscodedData(string username)
         {
-            None,
-            Latest,
-            Popular,
-            Top_Rated,
-            Upcoming
+            return Directory.CreateDirectory(Path.Combine(TempData, "transcoded", username)).FullName;
         }
 
-        public static class Paths
+        public static string MetaData => Directory.CreateDirectory(Path.Combine(Resources, "Metadata")).FullName;
+        public static string MovieData => Directory.CreateDirectory(Path.Combine(MetaData, "Movies")).FullName;
+        public static string TVData => Directory.CreateDirectory(Path.Combine(MetaData, "TV")).FullName;
+
+        public static string MissingPoster
         {
-            public static string Root => Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "LFInteractive", "Flexx")).FullName;
-            public static string Resources => Directory.CreateDirectory(Path.Combine(Root, "Resources")).FullName;
-            public static string Log => Directory.CreateDirectory(Path.Combine(Resources, "Logs")).FullName;
-            public static string Configs => Directory.CreateDirectory(Path.Combine(Resources, "Config")).FullName;
-            public static string UserData => Directory.CreateDirectory(Path.Combine(Resources, "UserData")).FullName;
-            public static string FFMpeg => Directory.CreateDirectory(Path.Combine(Resources, "FFMpeg")).FullName;
-            public static string TempData => Directory.CreateDirectory(Path.Combine(Resources, ".tmp")).FullName;
-
-            public static string TranscodedData(string username)
+            get
             {
-                return Directory.CreateDirectory(Path.Combine(TempData, "transcoded", username)).FullName;
-            }
-
-            public static string MetaData => Directory.CreateDirectory(Path.Combine(Resources, "Metadata")).FullName;
-            public static string MovieData => Directory.CreateDirectory(Path.Combine(MetaData, "Movies")).FullName;
-            public static string TVData => Directory.CreateDirectory(Path.Combine(MetaData, "TV")).FullName;
-
-            public static string MissingPoster
-            {
-                get
+                string path = Path.Combine(MetaData, "missing_poster.jpg");
+                if (!File.Exists(path))
                 {
-                    string path = Path.Combine(MetaData, "missing_poster.jpg");
-                    if (!File.Exists(path))
+                    HttpClient client = new();
+                    using HttpResponseMessage response = client.GetAsync($"https://flexx-tv.tk/assets/images/missing_poster.jpg").Result;
+                    if (response.IsSuccessStatusCode)
                     {
-                        new System.Net.WebClient().DownloadFile($"https://flexx-tv.tk/assets/images/missing_poster.jpg", path);
+                        using FileStream fs = new(path, FileMode.CreateNew, FileAccess.ReadWrite);
+                        response.Content.CopyToAsync(fs).Wait();
                     }
-                    return path;
                 }
+                return path;
             }
-
-            public static string MissingCover
-            {
-                get
-                {
-                    string path = Path.Combine(MetaData, "missing_cover.jpg");
-                    if (!File.Exists(path))
-                    {
-                        new System.Net.WebClient().DownloadFile($"https://flexx-tv.tk/assets/images/missing_cover.jpg", path);
-                    }
-                    return path;
-                }
-            }
-
-            public static string GetVersionPath(string metadata_directory, string title, int resolution, int bitrate) => Path.Combine(Directory.CreateDirectory(Path.Combine(metadata_directory, "versions")).FullName, $"{title}-{resolution}-{bitrate}K.mp4");
         }
 
-        public static class Functions
+        public static string MissingCover
         {
-            public static bool IsFileLocked(FileInfo file)
+            get
+            {
+                string path = Path.Combine(MetaData, "missing_cover.jpg");
+                if (!File.Exists(path))
+                {
+                    HttpClient client = new();
+                    using HttpResponseMessage response = client.GetAsync($"https://flexx-tv.tk/assets/images/missing_cover.jpg").Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        using FileStream fs = new(path, FileMode.CreateNew, FileAccess.ReadWrite);
+                        response.Content.CopyToAsync(fs).Wait();
+                    }
+                }
+                return path;
+            }
+        }
+
+        public static string GetVersionPath(string metadata_directory, string title, int resolution, int bitrate) => Path.Combine(Directory.CreateDirectory(Path.Combine(metadata_directory, "versions")).FullName, $"{title}-{resolution}-{bitrate}K.mp4");
+    }
+
+    public static class Functions
+    {
+        public static bool IsFileLocked(FileInfo file)
+        {
+            try
+            {
+                using FileStream stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+                stream.Close();
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+
+            //file is not locked
+            return false;
+        }
+
+        public static object GetJsonObjectFromURL(string url)
+        {
+            string json = "";
+            HttpClient client = new();
+            client.Timeout = new(0, 0, 20);
+            using (HttpResponseMessage response = client.GetAsync(url).Result)
             {
                 try
                 {
-                    using FileStream stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None);
-                    stream.Close();
+                    json = response.Content.ReadAsStringAsync().Result;
                 }
-                catch (IOException)
+                catch (Exception e)
                 {
-                    //the file is unavailable because it is:
-                    //still being written to
-                    //or being processed by another thread
-                    //or does not exist (has already been processed)
-                    return true;
-                }
-
-                //file is not locked
-                return false;
-            }
-
-            public static object GetJsonObjectFromURL(string url)
-            {
-                string json = "";
-                using (System.Net.WebClient client = new())
-                {
-                    try
-                    {
-                        json = client.DownloadString(url);
-                    }
-                    catch (Exception e)
-                    {
-                        log.Error($"Unable to fetch Json from url \"{url}\"", e);
-                        return new { };
-                    }
-                }
-                if (string.IsNullOrWhiteSpace(json))
-                {
-                    log.Error($"Unable to fetch Json from url \"{url}\"");
+                    log.Error($"Unable to fetch Json from url \"{url}\"", e);
                     return new { };
                 }
-                return JsonConvert.DeserializeObject(json);
             }
-
-            public static IEnumerable<T[]> SplitArray<T>(T[] fullArray, int size)
+            if (string.IsNullOrWhiteSpace(json))
             {
-                for (int i = 0; i < fullArray.Length; i += size)
-                {
-                    T[] range;
-                    try
-                    {
-                        range = fullArray.ToList().GetRange(i, Math.Min(size, fullArray.Length - i)).ToArray();
-                    }
-                    catch (ArgumentOutOfRangeException)
-                    {
-                        continue;
-                    }
-                    yield return range;
-                }
+                log.Error($"Unable to fetch Json from url \"{url}\"");
+                return new { };
             }
+            return JsonConvert.DeserializeObject(json);
         }
     }
 }
