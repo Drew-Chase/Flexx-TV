d0a10b22d153834ba76a00673c6cdbf85bc4d47c
Drew Chase <dcmanproductions@gmail.com>
2021-12-03 19:01:57 -0500

    Now will handle tv shows

--- Flexx.Server/Controllers/PostController.cs
+++ Flexx.Server/Controllers/PostController.cs
@@ -6,23 +6,44 @@ using Microsoft.AspNetCore.Mvc;
 namespace Flexx.Server.Controllers;
 
 [ApiController]
-[Route("/api/post/movies/")]
+[Route("/api/post/")]
 public class PostController : ControllerBase
 {
     #region Public Methods
 
     [HttpPost("watched")]
-    public IActionResult SetWatched([FromForm] string id, [FromForm] string library, [FromForm] string username, [FromForm] bool watched, [FromBody] int? season, [FromBody] int? episode)
+    public IActionResult SetWatched([FromForm] string id, [FromForm] string library, [FromForm] string username, [FromForm] bool watched, [FromForm] int? season, [FromForm] int? episode)
     {
+        User user = Users.Instance.Get(username);
+        if (library.Equals("tv"))
+        {
+            var tv = TvLibraryModel.Instance.GetShowByTMDB(id);
+            if (tv == null)
+                return BadRequest();
+            if (!season.HasValue && !episode.HasValue)
+            {
+                tv.MarkAsWatched(user);
+                return Ok();
+            }
+            else if (season.HasValue && !episode.HasValue)
+            {
+                var s = tv.GetSeasonByNumber(season.Value);
+                if (s == null)
+                    return BadRequest();
+                s.MarkAsWatched(user);
+                return Ok();
+            }
+        }
         MediaBase media = library.Equals("movie") ? MovieLibraryModel.Instance.GetMovieByTMDB(id) : library.Equals("tv") && season.HasValue && episode.HasValue ? TvLibraryModel.Instance.GetShowByTMDB(id).GetSeasonByNumber(season.Value).GetEpisodeByNumber(episode.Value) : null;
         if (media == null)
             return BadRequest(new { message = $"Movie with id of \"{id}\" does not exist" });
-        Users.Instance.Get(username).SetHasWatched(media, watched);
+        user.SetHasWatched(media, watched);
+        user.SetWatchedDuration(media, 0);
         return Ok();
     }
 
     [HttpPost("watched_duration")]
-    public IActionResult SetWatchedDuration([FromForm] string id, [FromForm] string library, [FromForm] string username, [FromForm] int duration, [FromBody] int? season, [FromBody] int? episode)
+    public IActionResult SetWatchedDuration([FromForm] string id, [FromForm] string library, [FromForm] string username, [FromForm] int duration, [FromForm] int? season, [FromForm] int? episode)
     {
         MediaBase media = library.Equals("movie") ? MovieLibraryModel.Instance.GetMovieByTMDB(id) : library.Equals("tv") && season.HasValue && episode.HasValue ? TvLibraryModel.Instance.GetShowByTMDB(id).GetSeasonByNumber(season.Value).GetEpisodeByNumber(episode.Value) : null;
         if (media == null)
