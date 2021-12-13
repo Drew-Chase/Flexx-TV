752612c36da2055c4c33b4c13c39b50c9614dd79
Drew Chase <dcmanproductions@gmail.com>
2021-12-13 03:22:09 -0500

    Added Firewall and Portforwarding Support

---
+++ Flexx.Core/Networking/Firewall.cs
@@ -0,0 +1,129 @@
+﻿using Open.Nat;
+using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using static Flexx.Data.Global;
+
+namespace Flexx.Networking;
+
+public static class Firewall
+{
+    #region Public Methods
+
+    public static async Task ClosePort(int port)
+    {
+        NatDevice device = await new NatDiscoverer().DiscoverDeviceAsync(PortMapper.Upnp, new(5000));
+
+        foreach (Mapping mapping in await device.GetAllMappingsAsync())
+        {
+            if (mapping.PrivatePort == port)
+            {
+                log.Debug($"Deleting {mapping}");
+
+                await device.DeletePortMapAsync(mapping);
+            }
+        }
+    }
+
+    public static IPAddress GetLocalIP()
+    {
+        NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+        foreach (NetworkInterface adapter in adapters)
+        {
+            IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+            GatewayIPAddressInformationCollection addresses = adapterProperties.GatewayAddresses;
+            if (addresses.Count > 0)
+            {
+                foreach (GatewayIPAddressInformation address in addresses)
+                {
+                    if (address.Address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        string root = $"{address.Address.ToString().Split('.')[0]}.{address.Address.ToString().Split('.')[1]}.{address.Address.ToString().Split('.')[2]}";
+                        IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+                        foreach (IPAddress ip in host.AddressList)
+                        {
+                            if (ip.AddressFamily == AddressFamily.InterNetwork)
+                            {
+                                if (ip.ToString().StartsWith(root))
+                                {
+                                    return ip;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        throw new WebException("No outward bound IP address was found");
+    }
+
+    public static IPAddress GetPublicIP()
+    {
+        try
+        {
+            return new NatDiscoverer().DiscoverDeviceAsync(PortMapper.Upnp, new(5000)).Result.GetExternalIPAsync().Result;
+        }
+        catch
+        {
+            try
+            {
+                return new NatDiscoverer().DiscoverDeviceAsync(PortMapper.Pmp, new(5000)).Result.GetExternalIPAsync().Result;
+            }
+            catch
+            {
+                throw new WebException("Couldn't find router using either UPNP or PMP protocols.");
+            }
+        }
+    }
+
+    public static async Task<bool> IsPortOpen(int port)
+    {
+        NatDiscoverer nat = new();
+        CancellationTokenSource cts = new(5000);
+        NatDevice device = await nat.DiscoverDeviceAsync(PortMapper.Upnp, cts);
+
+        foreach (Mapping mapping in await device.GetAllMappingsAsync())
+        {
+            if (mapping.PrivatePort == port)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static async Task ListPorts()
+    {
+        NatDiscoverer nat = new();
+        CancellationTokenSource cts = new(5000);
+        NatDevice device = await nat.DiscoverDeviceAsync(PortMapper.Upnp, cts);
+
+        foreach (Mapping mapping in await device.GetAllMappingsAsync())
+        {
+            log.Debug($"OPENED => {mapping}");
+        }
+    }
+
+    public static async Task OpenPort(int port, string description = "FlexxTV Media Server")
+    {
+        try
+        {
+            await ClosePort(port);
+            NatDevice device = await new NatDiscoverer().DiscoverDeviceAsync(PortMapper.Upnp, new(10000));
+            Mapping map = new(Protocol.Tcp, port, port, description);
+            await device.CreatePortMapAsync(map);
+            map = new(Protocol.Udp, port, port, description);
+            await device.CreatePortMapAsync(map);
+            log.Debug($"Created {map}");
+        }
+        catch (Exception e)
+        {
+            log.Error(e);
+        }
+    }
+
+    #endregion Public Methods
+}
---
+++ Flexx.Server/FlexxServer.cs
@@ -0,0 +1,116 @@
+﻿using Flexx.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using static Flexx.Data.Global;
+using System.Runtime.InteropServices;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Flexx.Server;
+
+public class FlexxServer
+{
+    #region Public Methods
+
+    public static void AddToFirewall()
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            Process process = new()
+            {
+                StartInfo = new()
+                {
+                    FileName = Paths.ExecutingBinary,
+                    Arguments = "-firewall",
+                    Verb = "runas",
+                    UseShellExecute = true,
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            if (!File.Exists(Path.Combine(Directory.GetParent(Paths.ExecutingBinary).FullName, "first_launch")))
+            {
+                log.Warn("Adding FlexxTV Media Server to Firewall");
+                AddToFirewall();
+            }
+
+            SetupProcess.Run().Wait();
+            ModifyWindow(true);
+            StartHttpServer().Wait();
+        }
+        else
+        {
+            foreach (var item in args)
+            {
+                switch (item)
+                {
+                    case "-firewall":
+                        FirewallManager.FirewallCom firewall = new();
+                        firewall.AddAuthorizeApp(new("FlexxTV Media Server", Paths.ExecutingBinary) { Enabled = true });
+                        if (!File.Exists(Path.Combine(Directory.GetParent(Paths.ExecutingBinary).FullName, "first_launch")))
+                            File.CreateText(Path.Combine(Directory.GetParent(Paths.ExecutingBinary).FullName, "first_launch")).Close();
+                        Environment.Exit(0);
+                        continue;
+                    default:
+                        continue;
+                }
+            }
+        }
+    }
+
+    public static void ModifyWindow(bool show)
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            [DllImport("kernel32.dll")]
+            static extern IntPtr GetConsoleWindow();
+
+            [DllImport("user32.dll")] static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+            const int SW_HIDE = 0; const int SW_SHOW = 5;
+
+            IntPtr handle = GetConsoleWindow();
+            ShowWindow(handle, show ? SW_SHOW : SW_HIDE);
+        }
+    }
+
+    private static Task StartHttpServer() =>
+            Task.Run(() =>
+        {
+            log.Debug("HTTP Server is Starting...");
+            Host.CreateDefaultBuilder()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseIISIntegration();
+                webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+                webBuilder.UseKestrel(options =>
+                {
+                    options.ListenAnyIP(config.ApiPort);
+                    new Process()
+                    {
+                        StartInfo = new()
+                        {
+                            FileName = $"http://127.0.0.1:{config.ApiPort}",
+                            Verb = "open",
+                            UseShellExecute = true,
+                        }
+                    }.Start();
+
+                    ModifyWindow(true);
+                });
+                webBuilder.UseStartup<Startup>();
+                log.Debug("HTTP Server is Now Active!!!");
+            }).Build().Run();
+        });
+
+    #endregion Public Methods
+}
