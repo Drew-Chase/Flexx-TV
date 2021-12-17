453d4e87bd4b175a815ea16ceb67e273f5571cca
Drew Chase <dcmanproductions@gmail.com>
2021-12-17 00:34:27 -0500

    added additional config values

--- Flexx.Core/Data/Configuration.cs
+++ Flexx.Core/Data/Configuration.cs
@@ -16,13 +16,16 @@ namespace Flexx.Data
 
         public Configuration()
         {
-            sysProfile = new(Path.Combine(Paths.Configs, "sys"), false);
+            sysProfile = new(Path.Combine(Paths.Configs, "sys"), true);
             sysProfile.Add("movies", "");
             sysProfile.Add("port", 3208);
             sysProfile.Add("tv", "");
             sysProfile.Add("next_scheduled_prefetch", "");
             sysProfile.Add("use_version_file", false);
             sysProfile.Add("language", "en");
+            sysProfile.Add("setup", false);
+            sysProfile.Add("portforward", false);
+            sysProfile.Add("token", "");
         }
 
         #endregion Public Constructors
@@ -36,6 +39,16 @@ namespace Flexx.Data
         public string MovieLibraryPath { get => sysProfile.GetConfigByKey("movies").Value; set => sysProfile.GetConfigByKey("movies").Value = value; }
 
         public string NextScheduledPrefetch { get => sysProfile.GetConfigByKey("next_scheduled_prefetch").Value; set => sysProfile.GetConfigByKey("next_scheduled_prefetch").Value = value; }
+
+        public bool PortForward { get => sysProfile.GetConfigByKey("portforward").Value; set => sysProfile.GetConfigByKey("portforward").Value = value; }
+
+        public bool Setup
+        {
+            get => sysProfile.GetConfigByKey("setup").Value;
+            set => sysProfile.GetConfigByKey("setup").Value = value;
+        }
+
+        public string Token { get => sysProfile.GetConfigByKey("token").Value; set => sysProfile.GetConfigByKey("token").Value = value; }
 
         public string TVLibraryPath { get => sysProfile.GetConfigByKey("tv").Value; set => sysProfile.GetConfigByKey("tv").Value = value; }
 
