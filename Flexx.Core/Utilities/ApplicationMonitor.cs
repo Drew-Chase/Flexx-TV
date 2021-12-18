c708ba02af065e07f7a86e6e976528619b6045ff
Drew Chase <dcmanproductions@gmail.com>
2021-12-18 13:37:04 -0500

    Added monitor for cpu, gpu, network, and memory usage

---
+++ Flexx.Core/Utilities/ApplicationMonitor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flexx.Utilities
+{
+    public static class ApplicationMonitor
+    {
+        #region Public Properties
+
+        public static object CurrentUsage => new
+        {
+            CPU = FlexxCPUUsage + "%",
+            GPU = FlexxGPUUsage + "%",
+            Network = FlexxNetworkUsage + "Kbps",
+            Memory = FlexxMemoryUsage + "MB",
+        };
+
+        #endregion Public Properties
+
+        #region Private Properties
+
+        private static double FlexxCPUUsage =>
+            Task.Run(async () =>
+            {
+                try
+                {
+                    DateTime startTime = DateTime.Now;
+                    TimeSpan startCPU = Process.GetCurrentProcess().TotalProcessorTime;
+                    await Task.Delay(500);
+                    return Math.Round((Process.GetCurrentProcess().TotalProcessorTime - startCPU).TotalMilliseconds / (Environment.ProcessorCount * (DateTime.Now - startTime).TotalMilliseconds) * 100, 2);
+                }
+                catch { }
+
+                return 0;
+            }).Result;
+
+        private static double FlexxGPUUsage => Math.Round(0d, 2);
+
+        private static double FlexxMemoryUsage => Math.Round(Process.GetCurrentProcess().PrivateMemorySize64 / Math.Pow(1024, 2), 2);
+
+        private static double FlexxNetworkUsage => Math.Round(0d, 2);
+
+        #endregion Private Properties
+    }
+}
