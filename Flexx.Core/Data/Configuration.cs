965e9c8eb388be703ecaf8dca0c4350b4021fa60
Drew Chase <dcmanproductions@gmail.com>
2022-01-04 15:27:55 -0500

    added options for missing poster and cover, aswell as prefetch options

--- Flexx.Core/Data/Configuration.cs
+++ Flexx.Core/Data/Configuration.cs
@@ -1,4 +1,5 @@
 ﻿using ChaseLabs.CLConfiguration.List;
+using System;
 using System.IO;
 using static Flexx.Data.Global;
 
@@ -20,12 +21,14 @@ namespace Flexx.Data
             sysProfile.Add("movies", "");
             sysProfile.Add("port", 3208);
             sysProfile.Add("tv", "");
-            sysProfile.Add("next_scheduled_prefetch", "");
+            sysProfile.Add("next_scheduled_prefetch", DateTime.Now.ToString("MM-dd-yyyy"));
             sysProfile.Add("use_version_file", false);
             sysProfile.Add("language", "en");
             sysProfile.Add("setup", false);
             sysProfile.Add("portforward", false);
             sysProfile.Add("token", "");
+            sysProfile.Add("missing_poster", "");
+            sysProfile.Add("missing_cover", "");
         }
 
         #endregion Public Constructors
@@ -35,6 +38,10 @@ namespace Flexx.Data
         public int ApiPort { get => sysProfile.GetConfigByKey("port").Value; set => sysProfile.GetConfigByKey("port").Value = value; }
 
         public string LanguagePreference { get => sysProfile.GetConfigByKey("language").Value; set => sysProfile.GetConfigByKey("language").Value = value; }
+
+        public string MissingCover { get => sysProfile.GetConfigByKey("missing_cover").Value; set => sysProfile.GetConfigByKey("missing_cover").Value = value; }
+
+        public string MissingPoster { get => sysProfile.GetConfigByKey("missing_poster").Value; set => sysProfile.GetConfigByKey("missing_poster").Value = value; }
 
         public string MovieLibraryPath { get => sysProfile.GetConfigByKey("movies").Value.Replace('\\', Path.DirectorySeparatorChar).Replace('/', Path.DirectorySeparatorChar); set => sysProfile.GetConfigByKey("movies").Value = value.Replace('\\', Path.DirectorySeparatorChar).Replace('/', Path.DirectorySeparatorChar); }
 
