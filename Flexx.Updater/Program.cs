97273a9a26218c2e3791b6f1f411c859e2e982fb
Drew Chase <dcmanproductions@gmail.com>
2022-02-25 12:19:00 -0500

    Added updater

---
+++ Flexx.Updater/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Flexx.Updater;
+
+public class Program
+{
+    #region Private Methods
+
+    private static void Main(string[] args)
+    {
+        if (args.Length == 0) return;
+        foreach (string arg in args)
+        {
+        }
+    }
+
+    #endregion Private Methods
+}
